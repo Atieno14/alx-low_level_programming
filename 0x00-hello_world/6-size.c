9f78c3c4e409d45252855b37d7153af3e4f40701
Atieno14 <sheronoduor@gmail.com>
2023-03-16 15:22:33 -0700

    example

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - this prints the size of various types on the computer it is compiled and run on
+ * main -  size of various types on the computer it is compiled and run on
  * Return: 0
  */
 int main(void)
