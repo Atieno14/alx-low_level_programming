b4da0c148d760416173e746c2cdfc05f8c9c97b0
Atieno14 <sheronoduor@gmail.com>
2023-03-16 15:50:36 -0700

    example

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,11 @@
  */
 
 int main(void)
+
 {
 
 printf("size of char: %zu byte(s)\n", sizeof(char));
+
 printf("size of int: %zu byte(s)\n", sizeof(int));
 printf("size of long int: %zu byte(s)\n", sizeof(long int));
 printf("size of long long int: %zu byte(s)\n", sizeof(long long int));
