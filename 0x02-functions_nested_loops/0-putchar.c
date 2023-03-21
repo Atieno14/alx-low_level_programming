f5efaf0f2a95a66612d6f76e7fb3852d93f122bc
Atieno14 <sheronoduor@gmail.com>
2023-03-21 07:19:42 -0700

    prints putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,13 @@
 #include "main.h"
-/**
- * main - program that prints _putchar
- * Return: 0
- */
+	/**
+	 * main - print _putchar
+	 * written by sharon
+	 * Return: 0
+	 */
 
-int main(void)
+
+	void _putchar();
+	
 {
 	_putchar('_');
 	_putchar('p');
@@ -17,4 +20,3 @@ int main(void)
 	_putchar('\n');
 	return (0);
 }
-
